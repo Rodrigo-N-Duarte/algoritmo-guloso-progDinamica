91333a4bc83d028d9dbaec900cc6ec9a6ed295d7
Matheus Trindade <matheust.rocha47@gmail.com>
2024-06-15 19:53:44 -0300

    algoritimo guloso atualizado para dar o retorno correto

--- backend/AlgoritmoDinamico.ts
+++ backend/AlgoritmoDinamico.ts
@@ -1,15 +1,15 @@
 import Cardapio from "./types/Cardapio";
-import {Prato} from "./types/Prato";
+import { Prato } from "./types/Prato";
 
 export default class AlgoritmoDinamico {
   calcularCardapio(cardapio: Cardapio) {
-    let {dias, orcamento, pratos} = cardapio;
+    let { dias, orcamento, pratos } = cardapio;
     let tabela: number[][] = new Array(dias + 1).fill(0).map(() => new Array(orcamento + 1).fill(0));
     let sequenciaIndexPratos: number[] = [];
 
     for (let i = 1; i <= dias; i++) {
       for (let j = 1; j <= orcamento; j++) {
-        let melhorPratoPossivel = this.descobrirMelhorPratoPossivel(pratos, j);
+        let melhorPratoPossivel = this.descobrirMelhorPratoPossivel(pratos, j, null, null);
         if (melhorPratoPossivel) {
           let lucro = melhorPratoPossivel.lucro;
           let custo = melhorPratoPossivel.custo;
@@ -36,13 +36,15 @@ export default class AlgoritmoDinamico {
     };
   }
 
-  descobrirMelhorPratoPossivel(pratos: Prato[], orcamento: number) {
+  descobrirMelhorPratoPossivel(pratos: Prato[], orcamento: number, pratoAnterior: Prato | null, pratoDoisAnterior: Prato | null) {
     let melhorPrato = null;
     let melhorDiferenca: number = -Infinity;
 
     for (const prato of pratos) {
       if (prato.custo <= orcamento) {
         let diferenca: number = (prato.lucro - prato.custo);
+        if (prato == pratoAnterior) diferenca = (prato.lucro * 0.5) - prato.custo
+        if (prato == pratoDoisAnterior) diferenca = 0
         if (diferenca > melhorDiferenca) {
           melhorDiferenca = diferenca;
           melhorPrato = prato;
@@ -50,5 +52,18 @@ export default class AlgoritmoDinamico {
       }
     }
     return melhorPrato;
+  }
+
+  verificarBody(cardapio: Cardapio): string | null {
+    if (!cardapio.dias || cardapio.dias < 1 || cardapio.dias > 21) {
+      return "Quantidade de dias inválida";
+    }
+    if (!cardapio.pratos || cardapio.pratos.length < 1 || cardapio.pratos.length > 50) {
+      return "Quantidade de pratos inválida";
+    }
+    if (cardapio.orcamento < 0 || cardapio.orcamento > 100) {
+      return "Orçamento inválido";
+    }
+    return null;
   }
 }
--- backend/types/Cardapio.ts
+++ backend/types/Cardapio.ts
@@ -4,4 +4,5 @@ export default interface Cardapio {
     dias: number,
     orcamento: number,
     pratos: Prato[]
+    
 }
