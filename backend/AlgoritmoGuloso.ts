8fad64185fa3b65530b05597c56700140f66797c
Rodrigo Duarte <duarte.n.rodrigo@gmail.com>
2024-06-15 17:04:42 -0300

    comentários guloso

--- backend/AlgoritmoGuloso.ts
+++ backend/AlgoritmoGuloso.ts
@@ -2,6 +2,11 @@ import {Prato} from "./types/Prato";
 import Cardapio from "./types/Cardapio";
 
 export default class AlgoritmoGuloso {
+    /*
+    Recebe um cardápio, com os dias, orçamento total e uma lista de pratos possíveis
+    Para cada dia faz uma análise do melhor prato possível para ser vendido e guarda em variáveis, pratos anteriores
+    Se não tiver nenhum prato disponível, retorna 0
+    * */
     calcularCardapio(cardapio: Cardapio) {
         let {dias, orcamento, pratos} = cardapio;
         let pratoAnterior: Prato | null = null, pratoDoisAnterior: Prato | null = null;
@@ -11,7 +16,6 @@ export default class AlgoritmoGuloso {
         for (let i = 0; i < dias; i++) {
             const melhorPratoPossivel = this.descobrirMelhorPratoPossivel(pratos, orcamento, pratoAnterior, pratoDoisAnterior);
             if (melhorPratoPossivel) {
-
                 orcamento -= melhorPratoPossivel.custo;
                 lucroFinal += melhorPratoPossivel.lucro;
 
@@ -31,6 +35,13 @@ export default class AlgoritmoGuloso {
         };
     }
 
+    /*
+    Método para descobrir o melhor prato possivel dentro a lista de pratos que caiba dentro do orçamento da vez
+    Uma repetição que percorre todo o array de pratos e memoriza a maior diferença entre lucro e custo dos pratos
+    A relação lucro/custo do prato pode mudar se o prato anterior ou de dois dias antes for igual
+    O método escolhe o melhor prato do momento, não calcula lucros futuros
+    No final retorna o prato possível com maior valor agregado
+    * */
     descobrirMelhorPratoPossivel(pratos: Prato[], orcamento: number, pratoAnterior: Prato | null, pratoDoisAnterior: Prato | null) {
         let melhorPrato = null;
         let melhorDiferenca: number = -Infinity;
@@ -49,6 +60,9 @@ export default class AlgoritmoGuloso {
         return melhorPrato;
     }
 
+    /*
+    Método usado apenas para chegar os dados do body da requisição
+    * */
     verificarBody(cardapio: Cardapio): string | null {
         if (!cardapio.dias || cardapio.dias < 1 || cardapio.dias > 21) {
             return "Quantidade de dias inválida";
